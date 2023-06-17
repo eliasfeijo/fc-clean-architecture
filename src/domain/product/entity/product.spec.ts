49002b0ba32ef251634477adf23d13bc89c5589b
C1320805 Elias Feijo de Almeida Pereira Lima <c1320805@interno.bb.com.br>
2023-06-17 08:50:45 -0300

    feat: product notification errors

--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -19,6 +19,14 @@ describe("Product unit tests", () => {
     }).toThrowError("Price must be greater than zero");
   });
 
+  it("should throw error when price is less than zero and name is empty", () => {
+    expect(() => {
+      const product = new Product("123", "", -1);
+    }).toThrowError(
+      "Product: Name is required,Product: Price must be greater than zero"
+    );
+  });
+
   it("should change name", () => {
     const product = new Product("123", "Product 1", 100);
     product.changeName("Product 2");
